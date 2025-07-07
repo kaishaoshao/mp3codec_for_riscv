432128aec246c3a0a9ad75022a200fdf9176e10a
kaishaoshao <2032751077@qq.com>
2025-07-07 08:41:35 +0800

    [build]: 添加或修改编译脚本(使用zcc)

---
+++ decode/build.sh
@@ -0,0 +1,2 @@
+zcc --target=riscv64-unknown-linux-gnu -O2 -DUNIX ./*.c -o mp3dec -lm
+
--- encode/build.sh
+++ encode/build.sh
@@ -1,1 +1,1 @@
-gcc -O2 -DUNIX -DBS_FORMAT=BINARY src/*.c -o mp3enc -lm
+zcc --target=riscv64-unknown-linux-gnu -O2  -std=c17 -DUNIX -DBS_FORMAT=BINARY src/*.c -o mp3enc -lm
